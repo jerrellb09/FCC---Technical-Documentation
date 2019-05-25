abb7e38f56b5e1ecb6220e155e9643c6d1e2dafb
Jerrell Boone <jboon001@fiu.edu>
2019-05-25 13:27:22 -0400

    First Commit
    Committing all files

---
+++ app.js
@@ -0,0 +1,20 @@
+var arr1 = [1,2,3,4,5,6,7]
+console.log(arr1)
+
+var arrNew = arr1.map(e => e * 2)
+console.log(arrNew)
+
+$(document).ready(function(){
+    // var item = $("#spider-man").val
+    console.log("item is being loaded")
+
+    $('.main-section').click(function(){
+        console.log("you've clicked here");
+    })
+
+
+
+    
+})
+
+
